115975de78fe00f242ca575f639d141f85b04650
Priyanshu <sidarsunita2@gmail.com>
2025-09-09 19:09:34 +0530

    navbar done

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div className="w-full h-full bg-stone-800">giw</div>;
+  return <div className="w-full h-screen bg-red-500">giw</div>;
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,20 +2,29 @@ import Link from "next/link";
 
 export const Navbar = () => {
   return (
-    <header className="py-8 fixed bottom-10 left-0 right-0 overflow-x-scroll bg-amber-300">
+    <header className="py-8 fixed bottom-10 left-0 right-0 overflow-x-scroll md:overflow-visible bg-[url(https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Altja_j%C3%B5gi_Lahemaal.jpg/1200px-Altja_j%C3%B5gi_Lahemaal.jpg)]">
       <nav className="flex justify-center gap-8 mx-8 whitespace-nowrap">
-        <Link href={"/#"} className="bg-blurfg-100 py-4 px-4 rounded-3xl glass">
+        <Link href={"/#"} className="bg-blurfg-100 py-4 px-4 rounded-4xl glass">
           HOME
         </Link>
 
         <Link
           href={"/#projects"}
-          className="py-2 px-4 bg-blur-100 backdrop-filter bg-opacity-0 backdrop-blur-3xl rounded-3xl"
+          className="py-4 px-4 bg-blurfg-100 glass rounded-4xl"
         >
           PROJECTS
         </Link>
-        <Link href={"/#exp"} className="py-2 px-4 glass rounded-3xl">
+        <Link
+          href={"/#exp"}
+          className="py-4 px-4 bg-blurfg-100 glass rounded-4xl"
+        >
           WORK EXPERIENCE
+        </Link>
+        <Link
+          href={"/#exp"}
+          className="py-4 bg-blurfg-100 px-4 glass rounded-4xl"
+        >
+          CONTACT
         </Link>
       </nav>
     </header>
