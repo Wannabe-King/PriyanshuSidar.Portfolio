339bf4ed0a0b0831ecc72d9620a427f12cc5bf04
Priyanshu <sidarsunita2@gmail.com>
2025-09-10 14:51:47 +0530

    navbar initial design complete

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,38 @@
-import { Ellipsis } from "lucide-react";
+import {
+  Ellipsis,
+  Github,
+  Instagram,
+  Mail,
+  MailOpen,
+  Twitter,
+} from "lucide-react";
 import Link from "next/link";
 
 export const Navbar = () => {
   return (
     <header
-      className="flex fixed bottom-10 left-0 right-0 overflow-x-scroll md:overflow-visible 
+      className="flex justify-center gap-4 fixed bottom-10 left-0 right-0 overflow-x-scroll md:overflow-visible 
     bg-[url(https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Altja_j%C3%B5gi_Lahemaal.jpg/1200px-Altja_j%C3%B5gi_Lahemaal.jpg)]
     "
     >
-      <div className="bg-blurfg-100 glass rounded-full p-2 sm:p-3">
-        <div className=" p-2 sm:p-4 bg-red-500 text-blurfg-100 rounded-full ">
+      <div className=" bg-blurfg-100 text-gray-700 glass rounded-full self-start hidden xs:block xs:mt-3 ">
+        <MailOpen size={25} className="m-4" fill="white" />
+      </div>
+      <div className=" bg-blurfg-100 text-gray-700 glass rounded-full self-start hidden xs:block xs:mt-3 ">
+        <Instagram size={25} className="m-4" fill="white" />
+      </div>
+      <div className="bg-blurfg-100 glass rounded-full p-2 xs:p-2.5">
+        <div className=" p-3 xs:p-4 bg-red-400 text-blurfg-100 rounded-full ">
           <Ellipsis />
         </div>
       </div>
-      <nav className="flex justify-center gap-8 mx-8 ">
+      <div className=" bg-blurfg-100 glass rounded-full self-start hidden xs:block xs:mt-3 ">
+        <Twitter size={25} className="m-4" fill="white" />
+      </div>
+      <div className=" bg-blurfg-100 glass rounded-full self-start hidden xs:block xs:mt-3 ">
+        <Github size={25} className="m-4" fill="white" />
+      </div>
+      {/* <nav className="flex justify-center gap-8 mx-8 ">
         <Link href={"/#"} className="bg-blurfg-100 py-4 px-4 rounded-4xl glass">
           HOME
         </Link>
@@ -36,7 +55,7 @@ export const Navbar = () => {
         >
           CONTACT
         </Link>
-      </nav>
+      </nav> */}
     </header>
   );
 };
