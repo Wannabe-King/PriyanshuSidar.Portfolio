3bb82d070647be6faa15ed8e6b6d084de056e08f
Priyanshu <sidarsunita2@gmail.com>
2025-09-14 00:37:00 +0530

    card added

---
+++ src/app/(landing)/components/card.tsx
@@ -0,0 +1,40 @@
+import { BoundingBox, motion } from "framer-motion";
+
+interface CardProp {
+  style: Object;
+  text?: string;
+  image?: string | Blob;
+  containerRef:
+    | false
+    | Partial<BoundingBox>
+    | {
+        current: Element | null;
+      };
+}
+
+const Card = ({ style, text, image, containerRef }: CardProp) => {
+  return image && !text ? (
+    <motion.img
+      className="absolute w-15 cursor-grab"
+      src={image}
+      style={style}
+      whileHover={{ scale: 1.05 }}
+      drag
+      dragConstraints={containerRef}
+      dragElastic={1}
+    />
+  ) : (
+    <motion.div
+      className="absolute px-1 py-4 text-xl text-center rounded-full ring ring-gray-700 font-extralight bg-storm w-[12rem] cursor-grab"
+      style={style}
+      whileHover={{ scale: 1.05 }}
+      drag
+      dragConstraints={containerRef}
+      dragElastic={1}
+    >
+      {text}
+    </motion.div>
+  );
+};
+
+export default Card;
--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,9 +1,11 @@
+import { About } from "./sections/about";
 import { Hero } from "./sections/hero";
 
 export default function Home() {
   return (
     <main className=" mx-auto max-w-7xl">
       <Hero />
+      <About />
     </main>
   );
 }
