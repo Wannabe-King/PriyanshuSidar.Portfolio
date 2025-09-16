102ef08babe9c46b8eb580ff0df43124f7b906e9
Priyanshu <sidarsunita2@gmail.com>
2025-09-17 04:34:41 +0530

    navbar fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,6 @@
-import { mySocials } from "@/lib/constant";
+"use client";
+
+import { motion } from "framer-motion";
 import {
   Ellipsis,
   Github,
@@ -9,80 +11,153 @@ import {
   Twitter,
 } from "lucide-react";
 import Link from "next/link";
+import { Dispatch, SetStateAction, useRef, useState } from "react";
 
 // TODO: Make navbar to appear on hover over the div
+type Position = {
+  left?: number;
+  width?: number;
+  opacity?: number;
+};
 
 export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [position, setPosition] = useState<Position>({
+    left: 0,
+    width: 0,
+    opacity: 0,
+  });
+
+  function switchState() {
+    setIsOpen(!isOpen);
+  }
+
   return (
     <header
       className="flex justify-center gap-4 fixed bottom-10 left-0 right-0 overflow-x-scroll md:overflow-visible z-10
     ]
     "
     >
-      <Link
-        className=" bg-blurfg-100 text-gray-600 glass rounded-full self-start hidden xs:block xs:mt-3 "
-        href={"#contact"}
+      {!isOpen && (
+        <Link
+          className=" bg-blurfg-100 text-blurfg-100 glass rounded-full self-start hidden xs:block xs:mt-3 hover:bg-white"
+          href={"#contact"}
+        >
+          <MailOpen size={25} className="m-4" fill="white" />
+        </Link>
+      )}
+      {!isOpen && (
+        <Link
+          className=" bg-blurfg-100 text-white glass rounded-full self-start hidden xs:block xs:mt-3 hover:bg-blue-500"
+          href={"https://www.linkedin.com/in/priyanshu-sidar-639914144/"}
+        >
+          <Linkedin size={25} className="m-4" fill="white" />
+        </Link>
+      )}
+      <div
+        className="bg-blurfg-100 glass rounded-full p-2 xs:p-2.5 font-bold"
+        onClick={switchState}
+        onMouseEnter={switchState}
+        onMouseLeave={switchState}
       >
-        <MailOpen size={25} className="m-4" fill="white" />
-      </Link>
-      <Link
-        className=" bg-blurfg-100 text-white glass rounded-full self-start hidden xs:block xs:mt-3 "
-        href={"https://www.linkedin.com/in/priyanshu-sidar-639914144/"}
-      >
-        <Linkedin size={25} className="m-4" fill="white" />
-      </Link>
-      <div className="bg-blurfg-100 glass rounded-full p-2 xs:p-2.5">
-        <div className=" p-3 xs:p-4 bg-red-400 text-blurfg-100 rounded-full ">
-          <Ellipsis />
-        </div>
+        {!isOpen && (
+          <div className=" p-3 xs:p-4 bg-red-400 text-blurfg-100 rounded-full">
+            <Ellipsis />
+          </div>
+        )}
+        {isOpen && (
+          <nav className={`flex justify-center mx-8 z-5`}>
+            <Tab name="HOME" href="/#" setPosition={setPosition} />
+            <Tab name="ABOUT" href="/#about" setPosition={setPosition} />
+            <Tab name="PROJECTS" href="/#projects" setPosition={setPosition} />
+            <Tab
+              name="WORK EXPERIENCE"
+              href="/#exp"
+              setPosition={setPosition}
+            />
+            <Tab name="CONTACT" href="/#contact" setPosition={setPosition} />
+            <Cursor position={position} />
+          </nav>
+        )}
       </div>
-      <Link
-        className=" bg-blurfg-100 text-white glass rounded-full self-start hidden xs:block xs:mt-3 "
-        href={"#"}
-      >
-        <Twitter size={25} className="m-4" fill="white" />
-      </Link>
-      <Link
-        className=" bg-blurfg-100 glass text-white rounded-full self-start hidden xs:block xs:mt-3 "
-        href={"https://github.com/Wannabe-King"}
-      >
-        <Github size={25} className="m-4" fill="white" />
-      </Link>
-      
-      
-      {/* FIX THIS */}
-
-
-      {/* <nav className="flex justify-center gap-8 mx-8 ">
-        <Link href={"/#"} className="bg-blurfg-100 py-4 px-4 rounded-4xl glass">
-          HOME
-        </Link>
-
+      {!isOpen && (
         <Link
-          href={"/#about"}
-          className="py-4 px-4 bg-blurfg-100 glass rounded-4xl"
+          className=" bg-blurfg-100 text-white glass rounded-full self-start hidden xs:block xs:mt-3 hover:bg-blue-400"
+          href={"#"}
         >
-          ABOUT
+          <Twitter size={25} className="m-4" fill="white" />
         </Link>
+      )}
+      {!isOpen && (
         <Link
-          href={"/#projects"}
-          className="py-4 px-4 bg-blurfg-100 glass rounded-4xl"
+          className=" bg-blurfg-100 glass text-white rounded-full self-start hidden xs:block xs:mt-3 hover:bg-black"
+          href={"https://github.com/Wannabe-King"}
         >
-          PROJECTS
+          <Github size={25} className="m-4" fill="white" />
         </Link>
-        <Link
-          href={"/#exp"}
-          className="py-4 px-4 bg-blurfg-100 glass whitespace-nowrap rounded-4xl"
-        >
-          WORK EXPERIENCE
-        </Link>
-        <Link
-          href={"/#contact"}
-          className="py-4 bg-blurfg-100 px-4 glass rounded-4xl"
-        >
-          CONTACT
-        </Link>
-      </nav> */}
+      )}
     </header>
+  );
+};
+
+const Tab = ({
+  name,
+  href,
+  setPosition,
+}: {
+  name: string;
+  href: string;
+  setPosition: Dispatch<SetStateAction<Position>>;
+}) => {
+  const ref = useRef<HTMLAnchorElement>(null);
+
+  return (
+    <Link
+      ref={ref}
+      href={href}
+      onMouseEnter={() => {
+        if (!ref?.current) return;
+
+        const { width } = ref.current.getBoundingClientRect();
+
+        setPosition({
+          left: ref.current.offsetLeft,
+          width,
+          opacity: 1,
+        });
+      }}
+      className="py-4 px-4  rounded-4xl  whitespace-nowrap  "
+    >
+      {name}
+    </Link>
+
+    // <li
+    //   ref={ref}
+    //   onMouseEnter={() => {
+    //     if (!ref?.current) return;
+
+    //     const { width } = ref.current.getBoundingClientRect();
+
+    //     setPosition({
+    //       left: ref.current.offsetLeft,
+    //       width,
+    //       opacity: 1,
+    //     });
+    //   }}
+    //   className="relative z-10 block cursor-pointer px-3 py-1.5 text-xs  mix-blend-difference md:px-5 md:py-3 md:text-base"
+    // >
+    //   {children}
+    // </li>
+  );
+};
+
+const Cursor = ({ position }: { position: Position }) => {
+  return (
+    <motion.li
+      animate={{
+        ...position,
+      }}
+      className="absolute -z-10 h-14 rounded-4xl bg-red-400 py-4 text-blurfg-100"
+    />
   );
 };
