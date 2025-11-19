5fdd7fd03b27987d59658e0bde853b27b51ec68c
Priyanshu <sidarsunita2@gmail.com>
2025-11-19 19:27:08 +0530

    resume added

--- src/app/(landing)/components/heroText.tsx
+++ src/app/(landing)/components/heroText.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { FlipWords } from "@/components/ui/flip-words";
+import { HoverBorderGradient } from "@/components/ui/hover-border-gradient";
 import { motion } from "framer-motion";
 
 export const HeroText = () => {
@@ -9,6 +10,14 @@ export const HeroText = () => {
     hidden: { opacity: 0, x: -50 },
     visible: { opacity: 1, x: 0 },
   };
+
+  const openResumeInNewPage = () => {
+    window.open(
+      "https://drive.google.com/file/d/10FMsYRKVTwrXCPV2Uh1pc9MvIki88dMA/view?usp=sharing",
+      "_blank"
+    );
+  };
+
   return (
     <div className="z-10 mt-20 text-center md:mt-40 md:text-left rounded-3xl bg-clip-text">
       {/* Desktop View */}
@@ -52,6 +61,19 @@ export const HeroText = () => {
           >
             Web & Mobile Solutions
           </motion.p>
+          <motion.div
+            onClick={openResumeInNewPage}
+            className="mt-10 py-4 px-16 my-4 bg-red-400 rounded-2xl"
+            variants={variants}
+            initial="hidden"
+            animate="visible"
+            transition={{ delay: 1.5 }}
+          >
+            Resume
+          </motion.div>
+          {/* <HoverBorderGradient className="w-100 h-10 bg-red-400">
+            Resume
+          </HoverBorderGradient> */}
         </div>
       </div>
       {/* Mobile View */}
@@ -95,6 +117,16 @@ export const HeroText = () => {
           >
             Mobile & Web Applications
           </motion.p>
+          <motion.div
+            onClick={openResumeInNewPage}
+            className="mt-5 py-3 my-4 w-40 mx-auto bg-red-400 rounded-2xl"
+            variants={variants}
+            initial="hidden"
+            animate="visible"
+            transition={{ delay: 1.5 }}
+          >
+            Resume
+          </motion.div>
         </div>
       </div>
     </div>
--- src/app/todayilearned/_components/learnedTile.tsx
+++ src/app/todayilearned/_components/learnedTile.tsx
@@ -2,7 +2,7 @@ export const LearnedTile = () => {
   return (
     <div className="">
       <div className=" bg-stone-200 text-blurfg-100 font-medium max-w-4xl">
-        Today's title
+        Today&apos;s title
       </div>
       <div>details</div>
       <div>code</div>
